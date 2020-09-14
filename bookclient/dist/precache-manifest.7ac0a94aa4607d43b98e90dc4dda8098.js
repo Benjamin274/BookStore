26801fd3106d47dbd8f3e33c083e1240876a0564
Benjamin <biniabera274@gmail.com>
2020-09-14 14:57:11 +0300

    STEP ADDED

---
+++ bookclient/dist/precache-manifest.7ac0a94aa4607d43b98e90dc4dda8098.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79f5252313df774d367f",
+    "url": "/css/app.3683d784.css"
+  },
+  {
+    "revision": "32c397439f26d60dfac6a173a99a65bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa8c0fdd34a11bf87a29",
+    "url": "/js/Genre.0efa4bc7.js"
+  },
+  {
+    "revision": "faba7dbffe893f385db9",
+    "url": "/js/about.7d5c7b2d.js"
+  },
+  {
+    "revision": "79f5252313df774d367f",
+    "url": "/js/app.dfd58bec.js"
+  },
+  {
+    "revision": "2959c454014e816e79a2",
+    "url": "/js/chunk-vendors.9e1ace67.js"
+  },
+  {
+    "revision": "df1d3a996e4605f1d1a7c98f4f239f4c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
